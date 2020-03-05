9e00156ed22de7bcaf0ad3a4cfa4e4ae537e2c6f
Jasmine Williams <jasminewilliams2017@gmail.com>
2020-03-05 13:56:45 -0600

    part one complete

--- Building.cs
+++ Building.cs
@@ -1,9 +1,51 @@
+using System;
+using System.Collections.Generic;
+
 namespace Planner {
 
   public class Building {
-    private string _designer = "Jasmine";
-    private DateTime _dateConstructed = 11 / 07 / 2019;
-    private string _address = "7201 Charlotte Pike";
-    private string _owner = "Ryan Del Casino LLC";
+    //Fields
+    private string _designer = "Jasmine Williams";
+    private DateTime _dateConstructed;
+    private string _address;
+    private string _owner;
+
+    //Properties
+
+    public int Stories { get; set; } = 3;
+    public double Width { get; set; } = 145.5;
+    public double Depth { get; set; } = 123.3;
+
+    //Computed Property
+    public double volume {
+      get {
+        return (Width * Depth) * (3 * Stories);
+      }
+    }
+
+    //Constructor
+    public Building (string address) {
+
+      _address = address;
+
+    }
+
+    //Methods
+    public void Construct () {
+      _dateConstructed = DateTime.Now;
+    }
+
+    public void Purchase (string name) {
+      _owner = name;
+    }
+
+    public void AddDevelopment () {
+      Console.WriteLine ($"{_address}");
+      Console.WriteLine ($"-----------------------");
+      Console.WriteLine ($"Designed by {_designer}");
+      Console.WriteLine ($"Constructed on {_dateConstructed}");
+      Console.WriteLine ($"Owned by {_owner}");
+      Console.WriteLine ($"{volume} cubic meters of space");
+    }
   }
 }
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Planner {
+    class Program {
+        static void Main (string[] args) {
+            Building cityHospital = new Building ("7201 Charlotte Pike");
+            cityHospital.Width = 76.3;
+            cityHospital.Depth = 43.5;
+            cityHospital.Stories = 4;
+
+            cityHospital.Construct ();
+            cityHospital.Purchase ("Steve");
+
+            cityHospital.AddDevelopment ();
+
+            Building cityBank = new Building ("2706 Acklen Avenue");
+            cityBank.Width = 85.4;
+            cityBank.Depth = 26.4;
+            cityBank.Stories = 6;
+
+            cityBank.Construct ();
+            cityBank.Purchase ("Adam");
+
+            cityBank.AddDevelopment ();
+
+            Building cityHall = new Building ("150 2nd Avenue North");
+            cityHall.Width = 78.3;
+            cityHall.Depth = 46.7;
+            cityHall.Stories = 3;
+
+            cityHall.Construct ();
+            cityHall.Purchase ("Rose");
+
+            cityHall.AddDevelopment ();
+
+            Building cityGrocery = new Building ("2807 22nd Avenue South");
+            cityGrocery.Width = 98.3;
+            cityGrocery.Depth = 34.2;
+            cityGrocery.Stories = 1;
+
+            cityGrocery.Construct ();
+            cityGrocery.Purchase ("Brenda");
+
+            cityGrocery.AddDevelopment ();
+
+            Building citySchool = new Building ("1506 Elmwood Avenue");
+            citySchool.Width = 99.2;
+            citySchool.Depth = 76.5;
+            citySchool.Stories = 2;
+
+            citySchool.Construct ();
+            citySchool.Purchase ("Jasmine");
+
+            citySchool.AddDevelopment ();
+
+        }
+    }
+}
