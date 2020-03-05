77352c410b31af28d031901455ef6eac44a04402
Jasmine Williams <jasminewilliams2017@gmail.com>
2020-03-05 12:58:21 -0600

    first commit

---
+++ Building.cs
@@ -0,0 +1,9 @@
+namespace Planner {
+
+  public class Building {
+    private string _designer = "Jasmine";
+    private DateTime _dateConstructed = 11 / 07 / 2019;
+    private string _address = "7201 Charlotte Pike";
+    private string _owner = "Ryan Del Casino LLC";
+  }
+}
